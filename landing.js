1982b79f5e064143d66807efaec16d08900998af
Madeline Pickering <madelinepickering@gmail.com>
2016-03-03 11:48:35 -0700

    connecting pages

--- landing.js
+++ landing.js
@@ -1,5 +1,11 @@
 $(function(){
   $(".here").on("click", function(){
-    window.location.href = "index.html";
+    $("html, body").animate({ scrollTop: "550px" });
+  })
+  $(".why").on("click", function(){
+    $("html, body").animate({ scrollTop: "1200px" });
+  })
+  $(".start").on("click", function(){
+    window.location.href = "http://madelinepick.github.io/fuelmapper.github.io/app.html";
   })
 })
