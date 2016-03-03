745344189209e542c1dc3f6612c45f5e7de100ed
Madeline Pickering <madelinepickering@gmail.com>
2016-03-03 15:45:26 -0700

    better data info

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(function(){
   $( "#fuelForm" ).on("submit", function(){
-    $("html, body").animate({ scrollTop: "600px" });
+    $("html, body").animate({ scrollTop: "620px" });
     $(".containing").css({"display": "flex"});
     $(".white").show();
     $(".top").show();
@@ -40,6 +40,8 @@ $(function(){
   forGraph.bikeburn = information.weight*information.bikepace*0.256032*0.5
 //average bike and run for easier data
   forGraph.groundburn = (forGraph.bikeburn+forGraph.runburn)/2
+  forGraph.totalburn = (forGraph.bikeburn+forGraph.runburn+forGraph.swimburn)/3
+  localStorage.storedData = JSON.stringify(forGraph);
   console.log(forGraph)
 forGraph.totaltime = Math.ceil(((information.swimpace/60)*2.4)+(112/information.bikepace)+(16/information.runpace))
 
@@ -63,6 +65,7 @@ for (var i = 1; i <= minusSwim+1; i++) {
   }
 }
 var fancyWidth = forGraph.totaltime*55.88;
+
 // bikeData += fancyWidth+","+400
 usefulArray[1].points = bikeData;
 
@@ -114,19 +117,16 @@ function triChart ( newPoints ) {
 }
 
 function circles (newCircles){
-  var circlePoints = newCircles
-
+  var circlePoints = newCircles;
   var fuel = d3Chart.container.selectAll("circle").data(circlePoints).enter().append("circle");
   var fuelAtts = fuel.attr("cx", function(d) { return d.x_axis;})
                         .attr("cy", function(d) { return d.y_axis;})
-                        .attr("r", "5")
-                        .attr("fill", "white");
-  var cap = d3Chart.container.selectAll("text").data(newCircles).enter().append("text");
-  var capAtts = cap.attr("x", function(d) { return d.x_axis;})
-                      .attr("y", function(d) { return d.y_axis;})
-                      .attr("class", "info")
-                      .attr("fill", "#333")
-                      .text(function(d) { return "Hour "+Math.round(Number(d.x_axis)/55.88)+":"+Math.round(2000 - Number(d.y_axis)*5)+" calories";});
+                        .attr("r", "10")
+                        .attr("fill", "white")
+  var showtotal = d3Chart.container.append("text")
+                                  .attr("x", "10")
+                                  .attr("y", "380")
+                                  .text("Your total time is "+forGraph.totaltime+"+ hours")
   }
 
 //make graph
@@ -135,7 +135,7 @@ d3Chart.circlesFn(inputCircles);
 
 //chart information
 
-var chartHTML = '<h3 class="infostyle">What is this graph saying?</h3><ul><h5 class="title1">Carbohydrate Calories</h5><li class="list1">First off, this graph is only showing immediately available carbohydrate calories, not calories stored as fat. Your body fuels endurance races from both sources, but you can continue to take in usable carbohydrates during a race, while you rely on existing fat sources.</li><h5 class="title2">Stored glycogen</h5><li class="list2">You will start off with an amount of stored glycogen based on your weight. Your stored glycogen is '+forGraph.stored +' calories.</li><h5 class="title3">Burn rate</h5><li class="list3">Each dip in the graph represent the carbohydrate calories you burn per hour, roughly half of your total calories burned. You burn '+Math.round(forGraph.swimburn*2)+' calories per hour while swimming, '+Math.round(forGraph.runburn*2)+' calories per hour while running and '+Math.round(forGraph.bikeburn*2)+' calories per hour while biking.</li><h5 class="title4">Fueling</h5><li class="list4">For our model, we used a standard 250 cals/hr fueling plan, which is why the graph shows a flat increase every hour.</li><h5 class="title5">My line goes off the chart!</h5><li class="list5">If your line dips below the bottom of the chart, this means your body would be relying on fat calories to finish the race since 250 cals per hour was not able to replace what you burned. Our advice is to up your calorie per hour intake (more than 250) if you see this happening in the graph.</li></ul>';
+var chartHTML = '<h3 class="infostyle">What is this graph saying?</h3><ul><h5 class="title1">Carbohydrate Calories&nbsp;<i class="fa fa-angle-down fa-2x"></i></h5><li class="list1">First off, this graph is only showing immediately available carbohydrate calories, not calories stored as fat. Your body fuels endurance races from both sources, but you can continue to take in usable carbohydrates during a race, while you rely on existing fat sources.</li><h5 class="title2">Stored glycogen</h5><li class="list2">You will start off with an amount of stored glycogen based on your weight. Your stored glycogen is '+forGraph.stored +' calories.</li><h5 class="title3">Burn rate</h5><li class="list3">Each dip in the graph represent the carbohydrate calories you burn per hour, roughly half of your total calories burned. You burn '+Math.round(forGraph.swimburn*2)+' calories per hour while swimming, '+Math.round(forGraph.runburn*2)+' calories per hour while running and '+Math.round(forGraph.bikeburn*2)+' calories per hour while biking.</li><h5 class="title4">Fueling</h5><li class="list4">For our model, we used a standard 250 cals/hr fueling plan, which is why the graph shows a flat increase every hour.</li><h5 class="title5">My line goes off the chart!</h5><li class="list5">If your line dips below the bottom of the chart, this means your body would be relying on fat calories to finish the race since 250 cals per hour was not able to replace what you burned. Our advice is to up your calorie per hour intake (more than 250) if you see this happening in the graph.</li></ul>';
 
 $(".chartInfo").append(chartHTML)
 
@@ -159,13 +159,13 @@ $( ".title5" ).on('click', function(){
 var fuelArray = [];
 information.chocolate === "chocolate" ? fuelArray.push("<div class='fuel chocolate'><img src='http://ecx.images-amazon.com/images/I/61pS2DKL8wL._SY355_.jpg'><p class='caption'>160 calories<br>Organic<br>Energy from Honey</p></div>", "<div class='fuel chocolate'><img src='http://www.tennisexpress.com/prodimages/35095-DEFAULT-l.jpg'><p class='caption'>250 calories<br>10g protein<br>Organic oats</p></div>" ) : null
 
-information.berry === "berry" ? fuelArray.push("<div class='fuel berry'><img src='http://ep.yimg.com/ay/trisports/clif-shot-bloks-44.jpg'><p class='caption'>200 calories<br>95% organic<br>50mg caffeine</p></div>", "<div class='fuel berry'><img src='https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRF2j2q-Mj2-olW3e6ognHTsZo59-YCe9zOq5XZc0ZNwdNPcn-m-g'><p class='caption'>35 calores per 8 oz.<br>160 mg sodium<br>Maximize fliud absorption</p></div>") : null
+information.berry === "berry" ? fuelArray.push("<div class='fuel berry'><img src='http://ep.yimg.com/ay/trisports/clif-shot-bloks-44.jpg'><p class='caption'>200 calories<br>95% organic<br>50mg caffeine</p></div>", "<div class='fuel berry'><img src='https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRF2j2q-Mj2-olW3e6ognHTsZo59-YCe9zOq5XZc0ZNwdNPcn-m-g'><p class='caption'>35 calories per 8 oz.<br>160 mg sodium<br>Maximize fluid absorption</p></div>") : null
 
 information.citrus === "citrus" ? fuelArray.push("<div class='fuel citrus'><img src='http://images.mec.ca/fluid/customers/c822/5036-528/generated/5036-528_NOC02_view1_1000x1000.jpg'><p class='caption'>160 calories<br>Organic<br>Energy from Honey</p></div>", "<div class='fuel citrus'><img src='http://www.bhphotovideo.com/images/images1000x1000/gu_energy_labs_gu_123043_gu_energy_gel_24_pack_1174362.jpg'><p class='caption'>100 calories<br>55mg sodium<br>20mg caffeine</p></div>") : null
 
 information.coffee === "coffee" ? fuelArray.push("<div class='fuel coffee'><img src='http://www.bhphotovideo.com/images/images1000x1000/gu_energy_labs_gu_123050_gu_energy_gel_24_pack_1174367.jpg'><p class='caption'>100 calories<br>60mg sodium<br>40mg caffeine</p></div>", "<div class='fuel coffee'><img src='https://thefeed.com/assets/Smooth-Caffeinator-400x400.jpg'><p class='caption'>200 calories<br>7g protein<br>Energy from dates</p></div>") : null
 
-information.lemonline === "lemonline" ? fuelArray.push("<div class='fuel lemonlime'><img src='http://cdn.shopify.com/s/files/1/0121/9362/products/XLLB1000_silver_web.jpg?v=1444841410'><p class='caption'>80 calories per 16oz.<br>360mg sodium<br>Only real fruit flavor</p></div>", "<div class='fuel lemonlime'><img src='http://www.sportbeans.com/assets/images/productImages/lime_package.png'><p class='caption'>100 calories<br>Vitamins C and B<br>Natural flavors</p></div>") : null
+information.lemonline === "lemonlime" ? fuelArray.push("<div class='fuel lemonlime'><img src='http://cdn.shopify.com/s/files/1/0121/9362/products/XLLB1000_silver_web.jpg?v=1444841410'><p class='caption'>80 calories per 16oz.<br>360mg sodium<br>Only real fruit flavor</p></div>", "<div class='fuel lemonlime'><img src='http://www.sportbeans.com/assets/images/productImages/lime_package.png'><p class='caption'>100 calories<br>Vitamins C and B<br>Natural flavors</p></div>") : null
 
 information.fruit === "fruit" ? fuelArray.push("<div class='fuel fruit'><img src='http://scene7.targetimg1.com/is/image/Target/14652295?wid=480&hei=480'><p class='caption'>100 calories<br>24g carbohydrates<br>20% vitamin B6</p></div>", "<div class='fuel fruit'><img src='http://www.gcbparts.com/assets/images/hawley/FOOD0911.jpg'><p class='caption'>10 calories<br>Stay hydrated<br>Not for fueling</p></div>") : null
 
@@ -195,9 +195,11 @@ function shuffle(array) {
 shuffle(fuelArray);
 var pickedFuel = fuelArray.splice(0,5);
 var joinedFuel = pickedFuel.join("");
-
-$(".top").append("<h4 class='topsub'>Your total time is "+forGraph.totaltime+"+ hours"+"</h4>")
-$(".titlerow").append("<h2>Good Fuel Choices for you:</h2>")
+var prevData = JSON.parse(localStorage.storedData);
+var burnrate = Math.round(prevData.totalburn*2);
+// Math.round(forGraph.totalburn*2);
+$(".top").append("<h5 class='little'>Hover over the data points to view your available calories</h5>")
+$(".titlerow").append("<h3 class='middle'>You burn roughly "+burnrate+" calories per hour! Refuel yourself with:</h3>")
 $(".foodrow").append(joinedFuel);
 
 $(".fuel").on("mouseenter", function(){
@@ -206,20 +208,38 @@ $(".fuel").on("mouseenter", function(){
 $(".fuel").on("mouseleave", function(){
   $(this).find('.caption').css({"visibility": "hidden"});
 })
-$("text").on("mouseenter", function(){
-  $(this).css({"opacity": "1"});
+
+$("circle").on("mouseenter", function(){
+  var circleX = "Hour "+Math.round(Number($(this).attr("cx"))/55.88);
+  var circlyY = ": "+Math.round(2000-Number($(this).attr("cy"))*5)+" calories";
+  var circlespot = circleX+circlyY;
+  var edge = Number(d3Chart.container.attr("width"));
+  console.log(circleX);
+  console.log(circlyY);
+  var box = d3Chart.container.append("rect")
+  var boxAtts = box.attr("x", parseInt(edge-200))
+                    .attr("y", "20")
+                    .attr("fill", "white")
+                    .attr("width", "300px")
+                    .attr("height", "50px")
+  var cap = d3Chart.container.append("text");
+  var capAtts = cap.attr("x", parseInt(edge-180))
+                      .attr("y", "50")
+                      .attr("class", "info")
+                      .attr("fill", "#333")
+                      .text(circlespot);
 })
-$("text").on("mouseleave", function(){
-  $(this).css({"opacity": "0"});
-})
-localStorage.forGraph = JSON.stringify(forGraph);
+$("circle").on("mouseleave", function(){
+  $(".info").remove();
+  $("rect").remove();
 })
 
+  })
   $(".clear").on("click", function(){
     location.reload();
   })
   $(".fueladvice").on("click", function(){
-    var prevData = JSON.parse(localStorage.forGraph);
-    console.log(prevData);
+    $("html, body").animate({ scrollTop: "1400px" });
   })
+
 })
